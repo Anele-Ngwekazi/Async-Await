78f5e683ff185f5f568a1544dcbc7010c0471bc5
Anele Ngwekazi <anele.ngwekazi@gmail.com>
2019-01-07 09:58:37 +0200

    first commit

---
+++ Async_Await/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncAwaitDemo demo = new AsyncAwaitDemo();
+            //demo.DoStuff().GetAwaiter().GetResult();
+
+            //for (int count = 0; count < 10; count++)
+            //{
+            //    Console.WriteLine("Main thread: " + count);
+            //}
+            string results = "nothing";
+            results = demo.LoadUrlContants();
+            Console.WriteLine(results);
+            Console.WriteLine("Read Complete");
+            Console.ReadKey();
+        }
+
+
+    }
+}
